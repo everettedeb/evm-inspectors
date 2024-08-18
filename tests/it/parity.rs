040cc6de26a8534bf201bc03ba6e3bbc182c1321
everettedeb <161762074+everettedeb@users.noreply.github.com>
2024-08-18 07:47:40 +0200

    Update parity.rs

--- tests/it/parity.rs
+++ tests/it/parity.rs
@@ -7,8 +7,7 @@ use revm::{
     db::{CacheDB, EmptyDB},
     interpreter::CreateScheme,
     primitives::{
-        BlockEnv, CfgEnv, CfgEnvWithHandlerCfg, EnvWithHandlerCfg, ExecutionResult, HandlerCfg,
-        Output, SpecId, TransactTo, TxEnv,
+        BlockEnv, CfgEnv, ExecutionResult, Output, SpecId, TransactTo, TxEnv,
     },
     DatabaseCommit,
 };
@@ -16,62 +15,49 @@ use revm_inspectors::tracing::{TracingInspector, TracingInspectorConfig};
 
 #[test]
 fn test_parity_selfdestruct() {
-    /*
-    contract DummySelfDestruct {
-        function close() public {
-            selfdestruct(payable(msg.sender));
-        }
-    }
-    */
-
-    // simple contract that selfdestructs when a function is called
+    // Simple contract that selfdestructs when a function is called
     let code = hex!("6080604052348015600f57600080fd5b50606a80601d6000396000f3fe6080604052348015600f57600080fd5b506004361060285760003560e01c806343d726d614602d575b600080fd5b603233ff5b00fea2646970667358221220e52c8372ad24b20a6f7e4a13772dbc1d00f7eb5d0934ed6d635d3f5bf47dbc9364736f6c634300080d0033");
 
     let deployer = Address::ZERO;
-
     let mut db = CacheDB::new(EmptyDB::default());
 
-    let cfg = CfgEnvWithHandlerCfg::new(CfgEnv::default(), HandlerCfg::new(SpecId::LONDON));
-
-    let env = EnvWithHandlerCfg::new_with_cfg_env(
-        cfg.clone(),
-        BlockEnv::default(),
-        TxEnv {
+    let cfg = CfgEnv::new_with_spec(SpecId::LONDON);
+    let env = {
+        let tx_env = TxEnv {
             caller: deployer,
-            gas_limit: 1000000,
+            gas_limit: 1_000_000,
             transact_to: TransactTo::Create(CreateScheme::Create),
             data: code.into(),
             ..Default::default()
-        },
-    );
+        };
+        CfgEnv::new_with_tx(cfg.clone(), BlockEnv::default(), tx_env)
+    };
 
     let mut insp = TracingInspector::new(TracingInspectorConfig::default_parity());
+    let (res, _) = inspect(&mut db, env, &mut insp).expect("Inspection failed");
 
-    let (res, _) = inspect(&mut db, env, &mut insp).unwrap();
     let addr = match res.result {
         ExecutionResult::Success { output, .. } => match output {
-            Output::Create(_, addr) => addr.unwrap(),
-            _ => panic!("Create failed"),
+            Output::Create(_, addr) => addr.expect("Address not found"),
+            _ => panic!("Unexpected output"),
         },
         _ => panic!("Execution failed"),
     };
     db.commit(res.state);
 
-    let mut insp = TracingInspector::new(TracingInspectorConfig::default_parity());
-
-    let env = EnvWithHandlerCfg::new_with_cfg_env(
-        cfg,
-        BlockEnv::default(),
-        TxEnv {
+    let env = {
+        let tx_env = TxEnv {
             caller: deployer,
-            gas_limit: 1000000,
+            gas_limit: 1_000_000,
             transact_to: TransactTo::Call(addr),
             data: hex!("43d726d6").into(),
             ..Default::default()
-        },
-    );
+        };
+        CfgEnv::new_with_tx(cfg, BlockEnv::default(), tx_env)
+    };
 
-    let (res, _) = inspect(&mut db, env, &mut insp).unwrap();
+    let mut insp = TracingInspector::new(TracingInspectorConfig::default_parity());
+    let (res, _) = inspect(&mut db, env, &mut insp).expect("Inspection failed");
     assert!(res.result.is_success());
 
     let traces = insp
@@ -80,74 +66,56 @@ fn test_parity_selfdestruct() {
         .into_localized_transaction_traces(TransactionInfo::default());
 
     assert_eq!(traces.len(), 2);
-    assert!(traces[1].trace.action.is_selfdestruct())
+    assert!(traces[1].trace.action.is_selfdestruct());
 }
 
-// Minimal example of <https://etherscan.io/tx/0xd81725127173cf1095a722cbaec118052e2626ddb914d61967fb4bf117969be0>
 #[test]
 fn test_parity_constructor_selfdestruct() {
-    // simple contract that selfdestructs when a function is called
-
-    /*
-    contract DummySelfDestruct {
-        function close() public {
-            new Noop();
-        }
-    }
-    contract Noop {
-        constructor() {
-            selfdestruct(payable(msg.sender));
-        }
-    }
-    */
+    // Simple contract with a constructor that selfdestructs
 
     let code = hex!("6080604052348015600f57600080fd5b5060b48061001e6000396000f3fe6080604052348015600f57600080fd5b506004361060285760003560e01c806343d726d614602d575b600080fd5b60336035565b005b604051603f90605e565b604051809103906000f080158015605a573d6000803e3d6000fd5b5050565b60148061006b8339019056fe6080604052348015600f57600080fd5b5033fffea264697066735822122087fcd1ed364913e41107ea336facf7b7f5972695b3e3abcf55dbb2452e124ea964736f6c634300080d0033");
 
     let deployer = Address::ZERO;
-
     let mut db = CacheDB::new(EmptyDB::default());
 
-    let cfg = CfgEnvWithHandlerCfg::new(CfgEnv::default(), HandlerCfg::new(SpecId::LONDON));
-
-    let env = EnvWithHandlerCfg::new_with_cfg_env(
-        cfg.clone(),
-        BlockEnv::default(),
-        TxEnv {
+    let cfg = CfgEnv::new_with_spec(SpecId::LONDON);
+    let env = {
+        let tx_env = TxEnv {
             caller: deployer,
-            gas_limit: 1000000,
+            gas_limit: 1_000_000,
             transact_to: TransactTo::Create(CreateScheme::Create),
             data: code.into(),
             ..Default::default()
-        },
-    );
+        };
+        CfgEnv::new_with_tx(cfg.clone(), BlockEnv::default(), tx_env)
+    };
 
     let mut insp = TracingInspector::new(TracingInspectorConfig::default_parity());
-    let (res, _) = inspect(&mut db, env, &mut insp).unwrap();
+    let (res, _) = inspect(&mut db, env, &mut insp).expect("Inspection failed");
+
     let addr = match res.result {
         ExecutionResult::Success { output, .. } => match output {
-            Output::Create(_, addr) => addr.unwrap(),
-            _ => panic!("Create failed"),
+            Output::Create(_, addr) => addr.expect("Address not found"),
+            _ => panic!("Unexpected output"),
         },
         _ => panic!("Execution failed"),
     };
     db.commit(res.state);
     print_traces(&insp);
 
-    let mut insp = TracingInspector::new(TracingInspectorConfig::default_parity());
-
-    let env = EnvWithHandlerCfg::new_with_cfg_env(
-        cfg,
-        BlockEnv::default(),
-        TxEnv {
+    let env = {
+        let tx_env = TxEnv {
             caller: deployer,
-            gas_limit: 1000000,
+            gas_limit: 1_000_000,
             transact_to: TransactTo::Call(addr),
             data: hex!("43d726d6").into(),
             ..Default::default()
-        },
-    );
+        };
+        CfgEnv::new_with_tx(cfg, BlockEnv::default(), tx_env)
+    };
 
-    let (res, _) = inspect(&mut db, env, &mut insp).unwrap();
+    let mut insp = TracingInspector::new(TracingInspectorConfig::default_parity());
+    let (res, _) = inspect(&mut db, env, &mut insp).expect("Inspection failed");
     assert!(res.result.is_success());
     print_traces(&insp);
 
